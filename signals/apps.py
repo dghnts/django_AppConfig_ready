5951cdccd168c4ca46fc0f59a94c80f01ba018ac
dghnts <mittsulann73@gmail.com>
2023-12-19 10:37:19 +0900

    add ready method

--- signals/apps.py
+++ signals/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class SignalsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'signals'
+    ready_has_run = False
+    count = 0 
+    def ready(self):
+        if self.ready_has_run:return 
+        self.count += 1
+        print(self.count)
+        print("Django has stareted!")
+        ready_has_run = True
